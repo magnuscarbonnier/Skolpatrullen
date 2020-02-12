95bd84aec640c8e20a8673b3f85e2f19e5698062
Joel Felldin <joelfelldin@hotmail.com>
2020-02-12 13:35:21 +0100

    add address, city and postal code to user

--- Skolpatrullen/Database/Models/User.cs
+++ Skolpatrullen/Database/Models/User.cs
@@ -21,6 +21,11 @@ namespace Database.Models
         [Required]
         [Range(8,16)]
         public string Password { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        public string City { get; set; }
+        public int PostalCode { get; set; }
         public ICollection<CourseParticipant> CourseParticipants { get; set; }
         public ICollection<UserSchool> UserSchools { get; set; }
     }
