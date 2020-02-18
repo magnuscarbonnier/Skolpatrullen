0d54db35f234c8bd53e9333ecba5ac707eaf5f00
Linus Alpsten <linus.alpsten@telia.com>
2020-02-18 19:05:58 +0100

    AppController

--- Skolpatrullen/WebApp/Controllers/AppController.cs
+++ Skolpatrullen/WebApp/Controllers/AppController.cs
@@ -7,26 +7,41 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using WebAPI;
+using WebApp.ViewModels;
 
 namespace WebApp.Controllers
 {
     public abstract class AppController : Controller
     {
         private readonly HttpClient HttpClient;
-        private readonly User User;
+        private User User;
         public AppController()
         {
             HttpClient = new HttpClient();
         }
 
-        public void GetUser()
+        public async void GetUser()
         {
             KeyValuePair<string,string>? cookie = Request.Cookies.SingleOrDefault(c => c.Key == "LoginToken");
             if (cookie.Value.Value == null)
             {
                 throw new Exception("Inte inloggad");
             }
-
+            var response = await APIGetLoginSession(cookie.Value.Value);
+            if (response.Success)
+            {
+                User = response.Data.User;
+            }
+            else
+            {
+                string errorMessage = "";
+                foreach (string em in response.ErrorMessages)
+                {
+                    errorMessage += em + "\n";
+                }
+                throw new Exception(errorMessage);
+            }
         }
 
         public async Task<HttpResponseMessage> APIPost<T>(string route, T body)
@@ -34,9 +49,19 @@ namespace WebApp.Controllers
             var json = JsonConvert.SerializeObject(body);
             using (var stringContent = new StringContent(json, Encoding.UTF8, "application/json"))
             {
-                HttpResponseMessage response = await HttpClient.PostAsync("https://localhost:44367/User/Login", stringContent);
+                HttpResponseMessage response = await HttpClient.PostAsync("https://localhost:44367"+route, stringContent);
                 return response;
             }
+        }
+        public async Task<APIResponse<bool>> APILogin(LoginViewModel loginVM)
+        {
+            HttpResponseMessage response = await APIPost("/User/Login", loginVM);
+            return (APIResponse<bool>)JsonConvert.DeserializeObject(await response.Content.ReadAsStringAsync());
+        }
+        public async Task<APIResponse<LoginSession>> APIGetLoginSession(string token)
+        {
+            HttpResponseMessage response = await APIPost("/User/GetLoginSession", token);
+            return (APIResponse<LoginSession>)JsonConvert.DeserializeObject(await response.Content.ReadAsStringAsync());
         }
     }
 }
