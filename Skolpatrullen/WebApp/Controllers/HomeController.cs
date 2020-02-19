7fe9a3fa986f9ad9641eac9e85add2a8f5962c17
Linus Alpsten <linus.alpsten@telia.com>
2020-02-19 13:17:09 +0100

    use AppController

--- Skolpatrullen/WebApp/Controllers/HomeController.cs
+++ Skolpatrullen/WebApp/Controllers/HomeController.cs
@@ -9,28 +9,24 @@ using WebApp.Models;
 
 namespace WebApp.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : AppController
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        public async Task<IActionResult> Index()
         {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
+            string message = await GetUser();
             return View();
         }
 
-        public IActionResult Privacy()
+        public async Task<IActionResult> Privacy()
         {
+            string message = await GetUser();
             return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public async Task<IActionResult> Error()
         {
+            string message = await GetUser();
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
--- Skolpatrullen/WebApp/Controllers/RegisterController.cs
+++ Skolpatrullen/WebApp/Controllers/RegisterController.cs
@@ -11,28 +11,32 @@ using WebApp.ViewModels;
 
 namespace WebApp.Controllers
 {
-    public class RegisterController : Controller
+    public class RegisterController : AppController
     {
-        private readonly HttpClient _client;
-        public RegisterController()
-        {
-            _client = new HttpClient();
-        }
         [HttpGet]
         [Route("[controller]")]
-        public IActionResult RegisterPage()
+        public async Task<IActionResult> RegisterPage()
         {
+            string message = await GetUser();
+            if (User != null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
             return View();
         }
         [HttpPost]
         [Route("[controller]")]
         public async Task<IActionResult> RegisterPage(UserViewModel userVM)
         {
+            string message = await GetUser();
+            if (User != null)
+            {
+                return RedirectToAction("Index", "Home"); 
+            }
             if (!ModelState.IsValid)
             {
                 return View();
             }
-            HttpResponseMessage response;
             try
             {
                 if (!(userVM.Password == userVM.RePassword))
@@ -41,10 +45,11 @@ namespace WebApp.Controllers
                     userVM.RePassword = "";
                     return View(userVM);
                 }
-                var json = JsonConvert.SerializeObject(userVM.ToUser());
-                using (var stringContent = new StringContent(json, Encoding.UTF8, "application/json"))
+                var response = await APIRegister(userVM);
+                if (response.Data != null)
                 {
-                    response = await _client.PostAsync("https://localhost:44367/User/Register", stringContent);
+                    Response.Cookies.Append("LoginToken", response.Data.Token);
+                    return RedirectToAction("Index", "Home");
                 }
             }
             catch
