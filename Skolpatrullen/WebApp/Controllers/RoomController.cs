800055ac8a15413e3168ab4c6748f5e6893ffb1f
mikael.tallbo <mikael.tallbo@gmail.com>
2020-02-19 11:44:17 +0100

    Add room page

---
+++ Skolpatrullen/WebApp/Controllers/RoomController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WebApp.ViewModels;
+
+namespace WebApp.Controllers
+{
+    public class RoomController : Controller
+    {
+        private readonly HttpClient _client;
+
+        public RoomController()
+        {
+            _client = new HttpClient();
+        }
+
+        [HttpGet]
+        [Route("[controller]")]
+        public IActionResult AddRoomPage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("[controller]")]
+        public async Task<IActionResult> AddRoomPage(RoomViewModel roomVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            HttpResponseMessage response;
+            try
+            {
+                var json = JsonConvert.SerializeObject(roomVM.ToRoom());
+                using (var stringContent = new StringContent(json, Encoding.UTF8, "application/json"))
+                {
+                    response = await _client.PostAsync("https://localhost:44367/Room/Add", stringContent);
+                }
+            }
+            catch
+            {
+                //send to error?
+            }
+            return View();
+        }
+    }
+}
---
+++ Skolpatrullen/WebApp/ViewModels/RoomViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Database.Models;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.ViewModels
+{
+    public class RoomViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+        public string SchoolId { get; set; }
+        
+        public Room ToRoom()
+        {
+            return new Room
+            {
+                Name = this.Name,
+                SchoolId = Int32.Parse(this.SchoolId)
+            };
+        }
+    }
+}
