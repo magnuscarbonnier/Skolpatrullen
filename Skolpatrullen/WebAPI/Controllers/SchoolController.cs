45d5cd6ff695be4ce2e6336e31be10b0d23e1fec
mikael.tallbo <mikael.tallbo@gmail.com>
2020-02-19 12:21:06 +0100

    Added GetAllSchools endpoint

---
+++ Skolpatrullen/WebAPI/Controllers/SchoolController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Database.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    public class SchoolController : ControllerBase
+    {
+        private readonly Context _context;
+        private readonly ILogger<SchoolController> _logger;
+        public SchoolController(Context context, ILogger<SchoolController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("[controller]/GetAllSchools")]
+        public IEnumerable<School> GetAllSchools()
+        {
+            var schoollist = _context.Schools.OrderBy(s => s.Name).ToList();
+            return schoollist;
+        }
+    }
+}
