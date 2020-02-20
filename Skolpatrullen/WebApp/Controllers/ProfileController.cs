d31bed59cec9fcff02d6d4abc526573f5ac7ea38
Joel Felldin <joelfelldin@hotmail.com>
2020-02-20 13:59:24 +0100

    Add-UserViewModel-to-returnviews

--- Skolpatrullen/WebApp/Controllers/ProfileController.cs
+++ Skolpatrullen/WebApp/Controllers/ProfileController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebApp.ViewModels;
 
 namespace WebApp.Controllers
 {
@@ -10,7 +11,7 @@ namespace WebApp.Controllers
     {
         public IActionResult ProfilePage()
         {
-            return View();
+            return View(new UserViewModel());
         }
     }
 }
