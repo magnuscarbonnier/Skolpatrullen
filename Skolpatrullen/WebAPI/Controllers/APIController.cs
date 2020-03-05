ce71010c365de1a18e86a0a0ad135a857ec22d7b
Linus Alpsten <linus.alpsten@telia.com>
2020-03-05 14:47:31 +0100

    add APIController

---
+++ Skolpatrullen/WebAPI/Controllers/APIController.cs
@@ -0,0 +1,21 @@
+﻿using Database.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    public class APIController : ControllerBase
+    {
+        public readonly Context _context;
+        public readonly ILogger<UserController> _logger;
+        public APIController(Context context, ILogger<UserController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+    }
+}
