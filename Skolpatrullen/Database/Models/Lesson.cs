442747461a05145245dec1d5ddd50605cf35d752
mikael.tallbo <mikael.tallbo@gmail.com>
2020-03-09 21:38:40 +0100

    add models and controller
    Add Lesson model
    Add LessonViewModel
    Add LessonController

---
+++ Skolpatrullen/Database/Models/Lesson.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Database.Models
+{
+    class Lesson
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public Course Course { get; set; }
+    }
+}
---
+++ Skolpatrullen/WebApp/ViewModels/LessonViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.ViewModels
+{
+    public class LessonViewModel
+    {
+        public int id { get; set; }
+        public string text { get; set; }
+        public string start_date { get; set; }
+        public string end_date { get; set; }
+        public string course { get; set; }
+    }
+}
