7b277e4a31ce9d3f1783ae2edf5213cf88e95569
Linus Alpsten <linus.alpsten@telia.com>
2020-03-10 14:09:17 +0100

    add passwordrecovery

---
+++ Skolpatrullen/Database/Migrations/20200310130641_add-passwordrecovery.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Database.Migrations
+{
+    public partial class addpasswordrecovery : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PasswordRecoveries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Token = table.Column<string>(nullable: true),
+                    UserId = table.Column<int>(nullable: false),
+                    ExpireTime = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PasswordRecoveries", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PasswordRecoveries_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PasswordRecoveries_UserId",
+                table: "PasswordRecoveries",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PasswordRecoveries");
+        }
+    }
+}
---
+++ Skolpatrullen/Database/Models/PasswordRecovery.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Database.Models
+{
+    public class PasswordRecovery
+    {
+        public int Id { get; set; }
+        public string Token { get; set; }
+        public int UserId { get; set; }
+        public DateTime ExpireTime { get; set; }
+        public User User { get; set; }
+    }
+}
