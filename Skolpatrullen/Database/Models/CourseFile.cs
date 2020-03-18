5d8b9811af20c34cce78d2ff787cd58490d2c206
mikael.tallbo <mikael.tallbo@gmail.com>
2020-03-18 08:24:14 +0100

    add coursefile model and prep for modal
    title

---
+++ Skolpatrullen/Database/Models/CourseFile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Database.Models
+{
+    class CourseFile
+    {
+        public string Name { get; set; }
+        File File { get; set; }
+        Course Course { get; set; }
+    }
+}
