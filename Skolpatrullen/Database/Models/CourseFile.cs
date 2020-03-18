a04a33482726c3d9542228c242352bc23c170867
mikael.tallbo <mikael.tallbo@gmail.com>
2020-03-18 08:35:30 +0100

    add properties
    add rest of properties to coursefile
    add coursefile in dbcontext

--- Skolpatrullen/Database/Models/CourseFile.cs
+++ Skolpatrullen/Database/Models/CourseFile.cs
@@ -4,9 +4,12 @@ using System.Text;
 
 namespace Database.Models
 {
-    class CourseFile
+    public class CourseFile
     {
+        public int Id { get; set; }
         public string Name { get; set; }
+        public int FileId { get; set; }
+        public int CourseId { get; set; }
         public File File { get; set; }
         public Course Course { get; set; }
     }
