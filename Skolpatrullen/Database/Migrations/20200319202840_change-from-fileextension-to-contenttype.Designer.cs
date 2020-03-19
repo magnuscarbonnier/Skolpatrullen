619842ae32744778d907b3a3a1be9bb579b86be9
Linus Alpsten <linus.alpsten@telia.com>
2020-03-19 21:32:11 +0100

    add migration

--- Skolpatrullen/Database/Migrations/20200319202840_change-from-fileextension-to-contenttype.Designer.cs
+++ Skolpatrullen/Database/Migrations/20200319202840_change-from-fileextension-to-contenttype.Designer.cs
@@ -4,14 +4,16 @@ using Database.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Database.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20200319202840_change-from-fileextension-to-contenttype")]
+    partial class changefromfileextensiontocontenttype
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Skolpatrullen/Database/Migrations/20200319202840_change-from-fileextension-to-contenttype.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Database.Migrations
+{
+    public partial class changefromfileextensiontocontenttype : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FileExtension",
+                table: "Files");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ContentType",
+                table: "Files",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ContentType",
+                table: "Files");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FileExtension",
+                table: "Files",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
