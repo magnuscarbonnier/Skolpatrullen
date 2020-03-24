b9a4df96e449a1aa8a2b96bcfb11f16c4fe7283f
magnuscarbonnier <magnus.carbonnier@gmail.com>
2020-03-24 12:40:24 +0100

    fix migration add api-controller

--- Skolpatrullen/Database/Migrations/20200324113733_CourseBlogFix.Designer.cs
+++ Skolpatrullen/Database/Migrations/20200324113733_CourseBlogFix.Designer.cs
@@ -4,14 +4,16 @@ using Database.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Database.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20200324113733_CourseBlogFix")]
+    partial class CourseBlogFix
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Skolpatrullen/WebAPI/Controllers/CourseBlogController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Database.Models;
+using Lib;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    public class CourseBlogController : APIController
+    {
+        public CourseBlogController(Context context, ILogger<UserController> logger) : base(context, logger)
+        {
+        }
+        [HttpGet]
+        [Route("[controller]/GetBlogPostsByCourseId/{id}")]
+        public APIResponse<IEnumerable<CourseBlogPost>> GetBlogPostsByCourseId(int id)
+        {
+            APIResponse<IEnumerable<CourseBlogPost>> response = new APIResponse<IEnumerable<CourseBlogPost>>();
+            var courseBlogList = _context.CourseBlogPosts.Where(bp => bp.CourseId == id).OrderBy(bp => bp.PublishDate).ToList();
+            if (courseBlogList != null)
+            {
+                response.Data = courseBlogList;
+                response.Success = true;
+                response.SuccessMessage = $"Hämtade alla inlägg för kurs med id: {id}";
+            }
+            return response;
+        }
+        //[HttpPost]
+        //[Route("[controller]/Add")]
+        //public APIResponse<Course> Add(Course course)
+        //{
+        //    APIResponse<Course> response = new APIResponse<Course>();
+        //    if (course != null)
+        //    {
+        //        if (course.StartDate > course.EndDate)
+        //        {
+        //            response.FailureMessage = $"Startdatum kan inte vara senare än slutdatum";
+        //            response.Success = false;
+        //        }
+        //        else
+        //        {
+        //            _context.Courses.Add(course);
+        //            _context.SaveChanges();
+        //            response.Data = course;
+        //            response.Success = true;
+        //            response.SuccessMessage = $"La till kurs med namn {course.Name}";
+        //        }
+        //    }
+        //    return response;
+        //}
+    }
+}
