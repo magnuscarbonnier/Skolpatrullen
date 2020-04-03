f01bfdc361e1ac6a7e54912a3cb332c5778fd446
mikael.tallbo <mikael.tallbo@gmail.com>
2020-04-03 09:38:17 +0200

    ef stuff
    new prop
    buncha ef stuff

--- Skolpatrullen/Database/Migrations/20200403073737_add_userassignment_fix.Designer.cs
+++ Skolpatrullen/Database/Migrations/20200403073737_add_userassignment_fix.Designer.cs
@@ -4,14 +4,16 @@ using Database.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Database.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20200403073737_add_userassignment_fix")]
+    partial class add_userassignment_fix
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
